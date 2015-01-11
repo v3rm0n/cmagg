6cb781fd72a3d8200c5cdc49e5dc94a53625d9cd
“Maido <maidokaara@gmail.com>
2015-01-11 12:52:08 +0200

    Github pages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,10 @@ require('require-dir')('./gulp');
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
+
+var deploy = require('gulp-gh-pages');
+
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(deploy());
+});
